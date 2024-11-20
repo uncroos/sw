9165f0ba168fb1991f9c1a48f5e1789eb5840bcb
uncroos <any492266@gmail.com>
2024-11-20 17:41:06 +0900

    Create py_3.py

---
+++ FileOrganization/py_3.py
@@ -0,0 +1,9 @@
+import os
+
+# 데이터가 저장된 폴더 경로
+folder_path = '/Users/an-yohan/Documents/GitHub/SW/data/train'
+
+# 폴더 내의 파일 목록을 가져오고 파일 수를 셉니다.
+file_count = len([file for file in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, file))])
+
+print(f"폴더 내 파일 수: {file_count}")
