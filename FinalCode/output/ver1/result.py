bda4fa2d6ad8aa8b1a759d59d35f99acd6649854
uncroos <any492266@gmail.com>
2024-11-21 08:15:44 +0900

    주석 수정

--- FinalCode/output/ver1/result.py
+++ FinalCode/output/ver1/result.py
@@ -6,7 +6,7 @@ data = pd.read_csv('/Users/an-yohan/Documents/GitHub/SW/data/output/submission.c
 # 'real'과 'fake'를 비교하여 'result' 열 생성
 data['result'] = (data['real'] > data['fake']).astype(int)
 
-# 결과 저장 (필요한 경우)
+# 결과 저장
 data.to_csv('submission_with_result.csv', index=False)
 
 # 처리된 데이터 확인
